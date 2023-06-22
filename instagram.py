3571ac75bdcb6a5beae755df7054bdb048008a8c
Edwardshandy <edward.shandy@prieds.com>
2023-06-22 22:26:59 +0700

    adding validation and refresh

--- instagram.py
+++ instagram.py
@@ -11,26 +11,32 @@ class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.username = "automation.edward"
+        self.password = "User1234"
+        self.refresh = self.driver.refresh()
 
     def test_login_page (self):
         driver = self.driver
         driver.get('https://instagram.com/')
         wait = WebDriverWait(driver,10)
+
         username_field = wait.until(EC.presence_of_element_located((By.NAME, 'username')))
-        username_field.send_keys("automation.edward")
+        username_field.send_keys(self.username)
 
         password_field = wait.until(EC.presence_of_element_located((By.NAME,'password')))
-        password_field.send_keys("User1234")
+        password_field.send_keys(self.password)
 
         button_login = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@type="submit"]')))
-        button_login.click
+        button_login.click()
 
-        wait.until(EC.text_to_be_present_in_element(By.XPATH, '//div[@class="_aagx"]'))
+        wait.until(EC.presence_of_element_located((By.XPATH, '//div[@class="_aagx"]')))
+        self.driver.refresh()
     
 
     def tearDown(self):
         time.sleep(5)
-        self.driver.quit()
+        # self.driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
